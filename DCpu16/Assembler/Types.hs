d59a6ef05794703bd50874d2644c22c3072a67b8
Aristid Breitkreuz <aristidb@googlemail.com>
2012-04-11 04:39:12 +0200

    representation fo Assembler Programs

--- DCpu16/Assembler/Types.hs
+++ DCpu16/Assembler/Types.hs
@@ -1,6 +1,11 @@
 module DCpu16.Assembler.Types where
 
-import DCpu16.Model
+import           DCpu16.Model
+
+import           Data.Sequence (Seq, (<|), (><), ViewL ((:<)))
+import qualified Data.Sequence as S
+import qualified Data.Map      as M
+import           Data.Monoid
 
 type Label = String
 
@@ -28,3 +33,32 @@ data AsmInstruction =
   , aiB :: AsmValue
   }
   deriving (Eq, Show)
+
+data AsmProgram 
+  = AsmProgram {
+      apAddressableBlocks :: Seq [AsmInstruction]
+    , apLabels :: M.Map Label Int
+    }
+  deriving (Eq, Show)
+
+prependBlock :: [AsmInstruction] -> AsmProgram -> AsmProgram
+prependBlock xs (AsmProgram bs lbls) = AsmProgram (xs <| bs) (fmap succ lbls)
+
+instance Monoid AsmProgram where
+  mempty = AsmProgram S.empty M.empty
+  mappend (AsmProgram as lblA) (AsmProgram bs lblB) = AsmProgram (as >< bs) (M.union (fmap (+ S.length as) lblB) lblB)
+
+prepareAsmProgram :: [Either Label AsmInstruction] -> AsmProgram
+prepareAsmProgram xs = let prog@(AsmProgram bs lbls) = go xs
+                           (i :< is) = S.viewl bs
+                       in case i of
+                            [] -> AsmProgram is (fmap pred lbls)
+                            _  -> prog
+  where
+    go :: [Either Label AsmInstruction] -> AsmProgram
+    go [] = AsmProgram ([] <| S.empty) M.empty
+    go (Left lbl : xs) = let (AsmProgram bs lbls) = go xs
+                         in AsmProgram ([] <| bs) (M.insert lbl 1 (fmap succ lbls))
+    go (Right instr : xs) = let (AsmProgram bs lbls) = go xs
+                                i :< is = S.viewl bs
+                            in AsmProgram ((instr : i) <| is) lbls
